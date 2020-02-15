9b24c2dddfa1eb4fa6f326b5d52aa20be1a63066
gesscavalcanti <61068372+gesscavalcanti@users.noreply.github.com>
2020-02-15 15:34:25 -0300

    Add files via upload

---
+++ avaliacao_1.py
@@ -0,0 +1,20 @@
+# scrapy runspider avaliacao_1.py -o av1_1.json --nolog
+
+import scrapy
+
+class UolSpider(scrapy.Spider):
+    name = 'Uol'
+
+    def __init__(self, *args, **kwargs):
+        super(UolSpider, self).__init__(*args, **kwargs)
+        self.start_urls = ['https://www.uol.com.br/']
+
+    def parse(self, response):
+        
+        dolar = response.xpath('//*[@id="HU_header"]/div[2]/div/div[2]/div[2]/ul/li[1]/a/span[2]/text()').extract_first()
+
+        print("Cotação do dolar: %s" % dolar)
+
+        yield {
+            'Cotação do dolar': dolar
+        }
